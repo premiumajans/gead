c1e78a27e2b515e964ca7f0e14063d748eb95214
premiumajans <mmcvakant@gmail.com>
2023-07-13 15:17:09 +0400

    nowrap

--- resources/views/backend/content/index.blade.php
+++ resources/views/backend/content/index.blade.php
@@ -17,7 +17,7 @@
                                 </a>
                             </div>
                         </div>
-                        <table id="datatable-buttons" class="table table-striped table-bordered dt-responsive nowrap"
+                        <table id="datatable-buttons" class="table table-striped table-bordered dt-responsive"
                                style="border-collapse: collapse; border-spacing: 0; width: 100%;">
                             <thead>
                             <tr>
